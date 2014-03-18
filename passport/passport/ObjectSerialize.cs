267f65406eabf5991d1b6bc163deaa3590a100a1
loganhong <loganhong@qq.com>
2014-03-19 00:11:31 +0800

    1

---
+++ passport/passport/ObjectSerialize.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace passport
+{
+    public static class ObjectSerialize
+    {
+        public static string SerializeToString<T>(T t) where T : class
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (XmlWriter sWriter = XmlWriter.Create(sb))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                serializer.Serialize(sWriter, t);
+            }
+            return sb.ToString();
+        }
+    }
+}
